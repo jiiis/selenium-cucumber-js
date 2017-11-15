931e8a007467e8145c39acd91904d80cf035aa6c
Hao Chang <yngwie.chang@gmail.com>
2017-11-16 10:34:24 +1100

    Removed geckodriver.

--- runtime/firefoxDriver.js
+++ runtime/firefoxDriver.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var firefox = require('geckodriver');
+// var firefox = require('geckodriver');
 var selenium = require('selenium-webdriver');
 
 /**
@@ -9,14 +9,14 @@ var selenium = require('selenium-webdriver');
  */
 module.exports = function() {
 
-    var driver = new selenium.Builder().withCapabilities({
-        browserName: 'firefox',
-        javascriptEnabled: true,
-        acceptSslCerts: true,
-        'webdriver.firefox.bin': firefox.path
-    }).build();
-
-    driver.manage().window().maximize();
-
-    return driver;
+    // var driver = new selenium.Builder().withCapabilities({
+    //     browserName: 'firefox',
+    //     javascriptEnabled: true,
+    //     acceptSslCerts: true,
+    //     'webdriver.firefox.bin': firefox.path
+    // }).build();
+    //
+    // driver.manage().window().maximize();
+    //
+    // return driver;
 };
